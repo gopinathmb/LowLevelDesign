ee23a064eded91951cf7b17eda1e5f88b40967e0
gopinath_mb <gopinathjnv@gmail.com>
2022-01-11 23:31:00 +0530

    Leetcode challenge Streams

--- src/main/java/com/concept/streams/StreamsTest.java
+++ src/main/java/com/concept/streams/StreamsTest.java
@@ -3,10 +3,20 @@
  */
 package com.concept.streams;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
 
 /**
  * @author gopinath_mb
  */
 public class StreamsTest {
 
+  public static void main(String[] args) {
+    List<Integer> nums = new ArrayList<>();
+
+    Collector res=null;
+    //    Collection<Integer> res = new ArrayList<>();
+    nums.stream().filter(i-> i%2==0).collect(res);
+  }
 }
